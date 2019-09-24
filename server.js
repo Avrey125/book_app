9138e17bc087988004c0beeb41bbddad5a1119f0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-09-24 13:39:15 -0700

    trying to render results

--- server.js
+++ server.js
@@ -16,48 +16,49 @@ app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get('/',(request, response) => {
-    response.render('pages/index');
+  response.render('pages/index');
 })
 
-app.post('/searches' , searchForBooks);
-app.post('/searches/show', searchForBooks)
+app.post('/searches', searchForBooks);
+// app.post('/searches/show', searchForBooks)
 
 
 function newSearch(request, response) {
-    console.log(request.body);
-    response.render('pages/index')
+  console.log(request.body);
+  response.render('pages/index')
 }
 
 function searchForBooks(request, response) {
-    console.log(request.body.search);
-    const searchName = request.body.search[0];
-    const searchingFor = request.body.search[1];
+  console.log(request.body.search);
+  const searchName = request.body.search[0];
+  const searchingFor = request.body.search[1];
 
-    let url = `https://www.googleapis.com/books/v1/volumes?q=`;
+  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
 
-    if(searchingFor === 'title'){
-        console.log('in first if');
-        url = url+`intitle:${searchName}`;
-    }
-    if(searchingFor === 'author'){
-        console.log('in second if');
-        url = url+`inauthor:${searchName}`;
-    }
+  if(searchingFor === 'title'){
+    console.log('in first if');
+    url = url+`intitle:${searchName}`;
+  }
+  if(searchingFor === 'author'){
+    console.log('in second if');
+    url = url+`inauthor:${searchName}`;
+  }
 
-    superagent.get(url)
+  superagent.get(url)
     .then(superagentResults => {
       const bookResults = (superagentResults.body.items.slice(0,10).map(book => {
         return new Book(book);
       }))
-      response.send(bookResults)
+
+      response.send(bookResults);
       console.log(superagentResults.body.items);
-        
     })
+    
 }
 
 
 function Book(info) {
-    this.title = info.volumeInfo.title || 'no title available';
+  this.title = info.volumeInfo.title || 'no title available';
 }
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
